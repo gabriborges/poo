995e903dd42e25d673c76a18d12d79be5b9cdd90
gabriborges <94412072+gabriborges@users.noreply.github.com>
2022-08-25 15:11:39 -0300

    Add files via upload

---
+++ atividade1.1/4.py
@@ -0,0 +1,4 @@
+nota=float(input("Nota: "))
+
+if nota<0 or nota>10:
+  print("Nota inválida")
---
+++ atividade1.1/5.py
@@ -0,0 +1,7 @@
+salario=float(input("Salario: "))
+prest=float(input("Prestação: "))
+
+if (salario*0.2)>prest:
+  print("Emprestimo Concedido")
+else:
+  print("Emprestimo não Concedido")
